9d3eec5d397d1762fdddf2bdbff50366d26ff091
Arthur Silva <arthurvieirasilva@gmail.com>
2020-08-08 12:12:28 -0300

    Inicia projeto

---
+++ AssetsMonitor/AssetsMonitor/Domain/Parameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetsMonitor.Domain
+{
+    class Parameters
+    {
+        
+        private string assetName;
+        private double minValue;
+        private double maxValue;
+
+        public string AssetName => assetName;
+        public double MinValue => minValue;
+        public double MaxValue => maxValue;
+
+        public Parameters(string assetName, double minValue, double maxValue)
+        {
+            this.assetName = assetName;
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+    }
+}
---
+++ AssetsMonitor/AssetsMonitor/Program.cs
@@ -0,0 +1,28 @@
+﻿using AssetsMonitor.Domain;
+using System;
+
+namespace AssetsMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string assetName = null;
+            double minValue = 0;
+            double maxValue = 0;
+            try
+            {
+                assetName = args[0].ToUpper();
+                minValue = Double.Parse(args[1]);
+                maxValue = Double.Parse(args[2]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro ao ler parâmetros, certifique-se que se encontram no formato: ATIVO VALOR_MAXIMO VALOR_MINIMO");
+                return;
+            }
+
+            Parameters p = new Parameters(assetName, minValue, maxValue);
+        }
+    }
+}
